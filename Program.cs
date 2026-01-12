92e7066e2c2cdc7181547c7fa73fcb20ebbf380e
dfabijani22 <dora.fabijanic@foi.hr>
2026-01-12 09:47:58 +0100

    https://boban.atlassian.net/browse/SCRUM-23 Postavljena osnovna struktura foldera, OpenAPI, Swagger UI

--- Program.cs
+++ Program.cs
@@ -6,13 +6,8 @@ using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -29,11 +24,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/va/swagger.json", "HomeBudget API v1");
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "HomeBudget API v1");
         options.RoutePrefix = "";
     });
 }
